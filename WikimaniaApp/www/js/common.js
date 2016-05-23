5bff9a4515948371641fd6488c3ade884c5fcc17
Pietro Martocchi <martocchi.pietro@outlook.com>
2016-05-23 22:49:05 +0200

    ora la larghezza del menu è responsive(il resto no)

--- WikimaniaApp/www/js/common.js
+++ WikimaniaApp/www/js/common.js
@@ -17,11 +17,12 @@
  * under the License.
  */
 
+var vw = window.innerWidth;
 var slideout = new Slideout({
     'panel': document.getElementById('panel'),
     'menu': document.getElementById('menu'),
-    'padding': 256,
-    'tolerance': 70
+    'padding': 3/10*vw,
+    'tolerance': (vw*1)/10
 });
 
 $(document).ready(function () {
