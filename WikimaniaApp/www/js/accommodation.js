7db3d968176faba150d04dd7364e9ad8ff11e4c5
david98 <megamanilsogno@gmail.com>
2016-05-27 22:24:25 +0200

    Mappa, ma il menu non va più sul telefono

--- WikimaniaApp/www/js/accommodation.js
+++ WikimaniaApp/www/js/accommodation.js
@@ -1,8 +1,44 @@
-﻿/*var request = loadExternalScript("http://www.openlayers.org/api/OpenLayers.js");
+﻿var request = loadExternalScript("http://www.openlayers.org/api/OpenLayers.js");
+
+$.ajax('loading.html', function (data, textStatus, jqXHR) {
+    $('.container').prepend(data);
+    loadCss('loading');
+});
 
 request.done(function () {
-    map = new OpenLayers.Map("map");
-    map.addLayer(new OpenLayers.Layer.OSM());
-    map.zoomToMaxExtent();
+
+    $('.loading').remove();
+
+    navigator.geolocation.getCurrentPosition(showMap, alert("Can't load the map!"));
 });
-*/
+
+function showMap(position) {
+    var lon = position.coords.longitude;
+    var lat = position.coords.latitude;
+    var zoom = 13;
+    var map = new OpenLayers.Map("map");
+    map.addLayer(new OpenLayers.Layer.OSM({
+        controls: [
+        new OpenLayers.Control.Navigation(),
+                   new OpenLayers.Control.ScaleLine(),
+                   new OpenLayers.Control.Permalink('permalink'),
+                   new OpenLayers.Control.MousePosition(),
+                   new OpenLayers.Control.Attribution()
+        ],
+        projection: new OpenLayers.Projection("EPSG:900913"),
+        displayProjection: new OpenLayers.Projection("EPSG:4326")
+    }));
+    map.zoomToMaxExtent();
+
+    var mapnik = new OpenLayers.Layer.OSM("OpenStreetMap (Mapnik)");
+
+    map.addLayer(mapnik);
+
+    var lonLat = new OpenLayers.LonLat(lon, lat)
+          .transform(
+            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+            map.getProjectionObject() // to Spherical Mercator Projection
+    );
+
+    map.setCenter(lonLat, zoom);
+}
