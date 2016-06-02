5469e7a19721fe97f3ac8e1fb290bd8fa8de09bc
Pietro Martocchi <martocchi.pietro@outlook.com>
2016-06-02 18:35:15 +0200

    bozza per i filtri aggiunta in "eventList"

---
+++ WikimaniaApp/www/js/eventList.js
@@ -0,0 +1,17 @@
+﻿var bntFilter, modal, btnCloseModal;
+
+$(document).ready(function () {
+    bntFilter = $('#bntFilter');
+    modal = $('#myModal');
+    btnCloseModal = $('#btnCloseModal');
+    bntFilter.bind('touchstart', openModal);
+    btnCloseModal.bind('touchstart', closeModal());
+});
+
+function openModal() {
+    modal.show();
+}
+
+function closeModal() {
+    modal.hide();
+}
